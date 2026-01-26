703d9b954f3fd492a59163508610c0f35dfcc3de
Pablix44737 <User@192.168.0.17>
2026-01-25 22:14:43 -0300

    separacion en carpetas (desktop y web) y config inicial de proyecto web

---
+++ web/TPI_colonia_felina_web_PAII/src/main/java/com/coloniafelina/tpi_colonia_felina_web_paii/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.coloniafelina.tpi_colonia_felina_web_paii;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
