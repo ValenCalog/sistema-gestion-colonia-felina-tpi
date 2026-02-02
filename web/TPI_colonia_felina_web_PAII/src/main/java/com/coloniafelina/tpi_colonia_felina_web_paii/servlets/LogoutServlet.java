b3f02d14e7f13f3a1f7c04926651329771f39da5
Valen <mvalentinacalogeropulos@gmail.com>
2026-02-02 18:35:17 -0300

    agregado el LogoutServlet para poder cerrar sesión

---
+++ web/TPI_colonia_felina_web_PAII/src/main/java/com/coloniafelina/tpi_colonia_felina_web_paii/servlets/LogoutServlet.java
@@ -0,0 +1,31 @@
+package com.coloniafelina.tpi_colonia_felina_web_paii.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/LogoutServlet"})
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.removeAttribute("usuarioLogueado");
+            session.invalidate();
+        }
+
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); 
+        response.setHeader("Pragma", "no-cache"); 
+        response.setDateHeader("Expires", 0); 
+
+        response.sendRedirect("index.jsp");
+    }
+}
